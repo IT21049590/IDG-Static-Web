69aac95a175c506cae2dce02d4eae29e13f20065
Hiruna Lalanga <IT21049590@my.sliit.lk>
2024-10-21 21:51:10 +0530

    modified config to work with github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['assets.aceternity.com', 'mysliit-my.sharepoint.com'],
-    },
-  };
+  images: {
+    domains: ['assets.aceternity.com', 'mysliit-my.sharepoint.com'],
+  },
+  output: 'export',
+  basePath: '/IDG-Static-Web', // Replace with your repository name
+  assetPrefix: '/IDG-Static-Web/',
+};
 
-  export default nextConfig;
+export default nextConfig;
