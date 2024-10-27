df996aa61c4985bff80e8a860137fce4af1ed69c
Hiruna Lalanga <IT21049590@my.sliit.lk>
2024-10-27 11:54:28 +0530

    css chaning

--- app/components/ProjectScope.tsx
+++ app/components/ProjectScope.tsx
@@ -84,7 +84,7 @@ const ProjectScope = () => {
                 >
                     Research Gap
                 </motion.h2>
-                <div className="w-1/2">
+                <div className="w-full">
                     <p className="text-gray-300">
                         Despite significant advancements in technology, gaps remain in leveraging digital tools to enhance essential inner development skills such as courage, critical thinking, communication, and co-creation.This research gap explores technological developments in four key areas of inner development: courage, critical thinking, communication, and co-creation skills
                     </p>
