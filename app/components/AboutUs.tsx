ba1c96065e4c34ff63467407dc540a102acb8668
Hiruna Lalanga <IT21049590@my.sliit.lk>
2024-10-27 11:07:12 +0530

    img change

--- app/components/AboutUs.tsx
+++ app/components/AboutUs.tsx
@@ -24,7 +24,7 @@ const AboutUs = () => {
                     </div>
                 </Card>
                 <Card>
-                    <CardImage src="./JennyMiss.JPG" alt="Team Member" />
+                    <CardImage src="/IDG-Static-Web/JennyMiss.JPG" alt="Team Member" />
                     <div className="p-4">
                         <CardTitle>Ms. Jenny Krishara</CardTitle>
                         <CardDescription>
